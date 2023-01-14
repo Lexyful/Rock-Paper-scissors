c2973c7fbd95a98b02b1bd65f0f64f05e1f2eef7
Alexys Jordan <alexyskjordan@gmail.com>
2023-01-14 10:19:55 -0700

    Feat player and computer picks

--- game.js
+++ game.js
@@ -1,51 +1,58 @@
 class Game {
-    constructor(gameChoice){
-        this.gameChoice = gameChoice
+    constructor(){
+        this.gameChoice
         this.human = new Player("human", "👩🏽")
         this.computer = new Player("computer", "🤖");
-        this.winner = winner
-        this.computerPick = null
+        this.winner
+        this.computerChoice = null
+        this.fighters
         
-}
+    } 
+
 
 pickFighters(){
     if(this.gameChoice === "Classic"){
         this.fighters = ["rock", "paper", "scissors"];
-    }else if(this.category === 'Spicy')
+    }else if(this.gameChoice === "difficult")
     {
         this.fighters = ["rock", "paper", "scissors", "chick", "hot-air-balloon"]
     }
 }
+
+
+
+getRandomWeapon() {
+    this.computerChoice = this.fighters[Math.floor(Math.random() * this.fighters.length)]
+    return this.computerChoice
 }
 
 
-function getRandomWeapon() {
-    this.computerPick = this.fighters[Math.floor(Math.random() * this.fighters.length)]
-    return this.computerPick
-}
-
-
-playGame(){
-    if(this.computerChoice === this.person.currentChoice){
-      this.winner = "Wah, Wah, Waaaaaaaaaaah... Draw"
+determineOutcome(){
+    console.log(this.human.currentChoice, this.computerChoice)
+    console.log( this.human.currentChoice === "rock" && this.computerChoice === "scissors")
+    if(this.computerChoice === this.human.currentChoice){
+        console.log("it's a tie")
+      this.winner = "Wah, Wah, Waaaaaaaaaaah...it's a Draw"
     }else if (
-      (this.person.currentChoice === "rock" && this.computerChoice === "scissor") ||
-      (this.person.currentChoice === "rock" && this.computerChoice === "hot-air-balloon") ||
-      (this.person.currentChoice === "paper" && this.computerChoice === "chick") ||
-      (this.person.currentChoice === "paper" && this.computerChoice === "rock") ||
-      (this.person.currentChoice === "scissor" && this.computerChoice === "paper") ||
-      (this.person.currentChoice === "scissor" && this.computerChoice === "hot-air-balloon") ||
-      (this.person.currentChoice === "hot-air-balloon" && this.computerChoice === "paper") ||
-      (this.person.currentChoice === "hot-air-balloon" && this.computerChoice === "chick") ||
-      (this.person.currentChoice === "chick" && this.computerChoice === "scissor") ||
-      (this.person.currentChoice === "chick" && this.computerChoice === "rock") ) {
-      this.winner =  "human"
-      this.person.wins ++
+      (this.human.currentChoice === "rock" && this.computerChoice === "scissors") ||
+      (this.human.currentChoice === "rock" && this.computerChoice === "hot-air-balloon") ||
+      (this.human.currentChoice === "paper" && this.computerChoice === "chick") ||
+      (this.human.currentChoice === "paper" && this.computerChoice === "rock") ||
+      (this.human.currentChoice === "scissors" && this.computerChoice === "paper") ||
+      (this.human.currentChoice === "scissors" && this.computerChoice === "hot-air-balloon") ||
+      (this.human.currentChoice === "hot-air-balloon" && this.computerChoice === "paper") ||
+      (this.human.currentChoice === "hot-air-balloon" && this.computerChoice === "chick") ||
+      (this.human.currentChoice === "chick" && this.computerChoice === "scissors") ||
+      (this.human.currentChoice === "chick" && this.computerChoice === "rock") ) {
+     console.log("human wins")
+        this.winner =  "human"
+      this.human.wins ++
     } else {
+        console.log("computer wins")
       this.winner = 'computer'
       this.computer.wins ++
     }
       
   }
 
-
+}
